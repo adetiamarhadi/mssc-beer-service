4fa62b0ff733407e284c2b2a088fa5d01f697e54
adetiamarhadi <adetia.marhadi@gmail.com>
2020-07-17 06:23:16 +0700

    Chapter 164 : Assignment - Database Initialization

--- src/main/java/com/github/adetiamarhadi/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/github/adetiamarhadi/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,64 +2,61 @@ package com.github.adetiamarhadi.msscbeerservice.bootstrap;
 
 import com.github.adetiamarhadi.msscbeerservice.domain.Beer;
 import com.github.adetiamarhadi.msscbeerservice.repositories.BeerRepository;
+import com.github.adetiamarhadi.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
-//@Component
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        //using data.sql file now
-        //loadBeerObjects();
+
+        if (beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
-        if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
+        Beer beer1 = beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("12.95"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
+        Beer beer2 = beerRepository.save(Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("12.95"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-        }
+        Beer beer3 = beerRepository.save(Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("11.95"))
+                .build());
 
-        System.out.println("Loader Beers: " + beerRepository.count());
+        beerRepository.save(beer1);
+        beerRepository.save(beer2);
+        beerRepository.save(beer3);
     }
 }
